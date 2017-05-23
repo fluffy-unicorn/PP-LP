42125e1040b274ca8b3754dbcb1a8d0c84cc1a6c
fluffy-unicorn <matthijsvhelden@gmail.com>
2017-05-23 19:49:38 +0200

    all exercises complete

--- treesort.pl
+++ treesort.pl
@@ -26,7 +26,20 @@ insert(nil,N,(t(nil,N,nil))).
 insert(t(L,M,R),N,t(S,M,R)) :- N < M, insert(L,N,S).
 insert(t(L,M,R),N,t(L,M,S)) :- N >= M, insert(R,N,S).
 
-listtree([],nil).
-listtree([X],t(nil,X,nil)).
-listtree([X|Xs],T2) :- listtree(Xs, T), insert(T,X,T2).
+deleteAll(T,N,R) :- insert(Tmp,N,T), deleteAll(Tmp,N,R).
+deleteAll(T,_,T).
+
+listtree(L,T) :- listtree(L,T,nil).
+listtree([],T,T).
+listtree([X|Xs],T,T1) :- insert(T1,X,T2), listtree(Xs,T,T2).
+
+treelist(T,L) :- treelist(T,L,[]).
+treelist(nil, L, L).
+treelist(t(Left,N,Right), L, Ltmp0) :- treelist(Right, Ltmp1, Ltmp0), append([N],Ltmp1,Ltmp2), treelist(Left, L, Ltmp2).
+
+treesort(Lin,Lout) :- listtree(Lin,T), treelist(T,Lout).
+
+testts1(R) :- treesort([1,5,3,6,2,7,4,22],R).
+testts2(R) :- treesort([53,32,65,34,31,23,435,23],R).
+testts3(R) :- treesort([1,6,4,6,3,4,6,3,2],R).
 
